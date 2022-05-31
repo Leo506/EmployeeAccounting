4497bbaf5471b3c5d1dc2beb89945d95fa06039c
Artem Levin <leoartemlev@yandex.ru>
2022-05-31 18:35:03 +0500

    Добавил страницу добавления нового сотрудника

---
+++ AddWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EmployeeAccounting
+{
+    /// <summary>
+    /// Логика взаимодействия для AddWindow.xaml
+    /// </summary>
+    public partial class AddWindow : Window
+    {
+        public AddWindow()
+        {
+            InitializeComponent();
+            ChoiceHead.IsEnabled = false;
+            DepartmentNameInput.IsEnabled = false;
+        }
+
+        private void OnTypeChange(object sender, SelectionChangedEventArgs e)
+        {
+            string type = ((TextBlock)TypeInput.SelectedItem).Text;
+            switch (type)
+            {
+                case "Работник":
+                    ChoiceHead.IsEnabled = true;
+                    break;
+
+                case "Руководитель":
+                    DepartmentNameInput.IsEnabled = true;
+                    ChoiceHead.IsEnabled = false;
+                    break;
+
+                default:
+                    ChoiceHead.IsEnabled = false;
+                    DepartmentNameInput.IsEnabled = false;
+                    break;
+            }
+        }
+    }
+}
